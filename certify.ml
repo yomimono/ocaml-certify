bc60696a65605f2738e766a8258168f2c208b7d9
Mindy Preston <meetup@yomimono.org>
2015-06-14 12:14:24 +0100

    ok, can make a ss cert now, yay.

--- certify.ml
+++ certify.ml
@@ -2,19 +2,18 @@ let () =
   let issuer = "my totally rad self-signed thing \o/" in
   let length = 2048 in
   let filename = "sweet_key.pem" in
-  let privkey = `RSA (Nocrypto.Rsa.generate length) in
-  let entropy_src = "/dev/random" in
-  let entropy src = 
-    let entropy_fd = Unix.openfile src [Unix.O_RDONLY] 0o777 in
-    let bytes = Bytes.create 1 in
-    let entropy = Unix.read entropy_fd bytes 0 1 in
+  let get_entropy how_much = 
+    let src = "/dev/urandom" in
+    let entropy_fd = Unix.openfile src [Unix.O_RDONLY] 0o000 in
+    let bytes = Bytes.create how_much in
+    let read_entropy = Unix.read entropy_fd bytes 0 how_much in
     let _ = Unix.close entropy_fd in
-    if entropy = 1 then Cstruct.of_string bytes else 
+    if (read_entropy = how_much) then (Cstruct.of_string bytes) else 
       raise (failwith "Couldn't read entropy!")
   in
   (* make and seed an Rng *)
-  let rng = Nocrypto.Fortuna.create () in
-  Nocrypto.Fortuna.reseed rng (entropy entropy_src);
+  Nocrypto.Rng.reseed (get_entropy 32);
+  let privkey = `RSA (Nocrypto.Rsa.generate length) in
   (* should be user-set too, but keep in mind we may get Invalid_argument for
                                           nonsense lengths *) 
   (* can extract the public privkey with Nocrypto.rsa.pub_of_priv *)
@@ -22,10 +21,9 @@ let () =
   let cert = X509.CA.sign (* at least let user change the valid period *) csr privkey issuer in
   let pem = X509.Encoding.Pem.Cert.to_pem_cstruct1 cert in
   (* make that be a file; how badly doeUnix.s that suck with Pervasives? *)
-  let fd = Unix.openfile filename [Unix.O_WRONLY] 0o277 in (* TODO: exc handling *)
+  let fd = Unix.openfile filename [Unix.O_WRONLY; Unix.O_CREAT] 0o600 in (* TODO: exc handling *)
   let _ = Unix.single_write fd (Cstruct.to_string pem) 0 (Cstruct.len pem) in
   (* seems kind of silly to get a return code when we're also supposed to get a bunch of
      Unix exceptions *)
   Unix.close fd;
   Printf.printf "key written to %s\n%!" filename
-
