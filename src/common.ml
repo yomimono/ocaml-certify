ecf4597d280647c9ef9c7994801dff173008f7fd
Mindy Preston <meetup@yomimono.org>
2015-06-23 11:22:11 +0100

    make csr binary as well; move code around a bit

--- src/common.ml
+++ src/common.ml
@@ -44,24 +44,6 @@ let write_pem dest pem =
   with
   | Unix.Unix_error (e, _, _) -> translate_error dest e
 
-let selfsign common_name length days certfile keyfile =
-  let (issuer : X509.component list) =
-    [ `CN common_name ]
-  in
-  let start,expire = make_dates days in
-  Nocrypto_entropy_unix.initialize ();
-  let privkey = `RSA (Nocrypto.Rsa.generate length) in
-  let csr = X509.CA.generate issuer privkey in 
-  (* default is sha256; TODO probably should allow user to choose sha1 as well *)
-  let cert = X509.CA.sign ~valid_from:start ~valid_until:expire
-      csr privkey issuer in
-  let cert_pem = X509.Encoding.Pem.Cert.to_pem_cstruct1 cert in
-  let key_pem = X509.Encoding.Pem.Private_key.to_pem_cstruct1 privkey in
-
-  match (write_pem certfile cert_pem, write_pem keyfile key_pem) with
-  | Ok, Ok -> `Ok
-  | Error str, _ | _, Error str -> Printf.eprintf "%s\n" str; `Error
-
 let length =
   let doc = "Length of the key in bits." in
   Arg.(value & opt int 2048 & info ["l"; "length"] ~doc)
@@ -82,19 +64,3 @@ let common_name =
   let doc = "Common name for which to issue the certificate." in
   Arg.(required & pos ~rev:false 0 (some string) None & info [] ~doc ~docv:"CN")
 
-
-let selfsign_t = Term.(pure selfsign $ common_name $ length $ days
-                       $ certfile $ keyfile )
-
-let info =
-  let doc = "generate a self-signed certificate" in
-  let man = [ `S "BUGS"; `P "Submit bugs at
-  https://github.com/yomimono/ocaml-certify";] in
-  Term.info "certify" ~doc ~man
-
-let () = 
-  match Term.eval (selfsign_t, info) with 
-  | `Help -> exit 0 (* TODO: not clear to me how we generate this case *)
-  | `Version -> exit 0  (* TODO: not clear to me how we generate this case *)
-  | `Error _ -> exit 1 
-  | `Ok _ -> exit 0
