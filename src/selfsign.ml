e5ea95c61cb464f0bb12650a8d9d57ea60978946
Stefan Grundmann <sg2342@googlemail.com>
2015-06-30 04:49:37 +0000

    always include `Subject_key_id in extensions

--- src/selfsign.ml
+++ src/selfsign.ml
@@ -1,28 +1,34 @@
 open Cmdliner
 open Common
 
-let ca_extensions =
-  [ (true, (`Basic_constraints (true, None)))
-  ; (true, (`Key_usage [ `Key_cert_sign
-                       ; `CRL_sign
-                       ; `Digital_signature
-                       ; `Content_commitment
-                       ]))
-  ]
-
 let selfsign common_name length days is_ca certfile keyfile =
   let (issuer : X509.component list) =
     [ `CN common_name ]
   in
   let start,expire = make_dates days in
   Nocrypto_entropy_unix.initialize ();
-  let privkey = `RSA (Nocrypto.Rsa.generate length) in
-  let ext = if is_ca then ca_extensions else [] in
-  let csr = X509.CA.request issuer privkey in
+  let privkey = Nocrypto.Rsa.generate length in
+  let ext =
+    let pubkey = Nocrypto.Rsa.pub_of_priv privkey in
+    let cs0 = X509.Encoding.rsa_public_to_cstruct pubkey in
+    let cs = Nocrypto.Hash.digest `SHA1 cs0 in
+    let subject_key_id = (false, (`Subject_key_id cs)) in
+    let ca_extensions =
+      if not is_ca then []
+      else
+        [ (true, (`Basic_constraints (true, None)))
+        ; (true, (`Key_usage [ `Key_cert_sign
+                             ; `CRL_sign
+                             ; `Digital_signature
+                             ; `Content_commitment
+                             ]))
+        ] in
+    subject_key_id :: ca_extensions in
+  let csr = X509.CA.request issuer (`RSA privkey) in
   let cert = X509.CA.sign ~valid_from:start ~valid_until:expire ~extensions:ext
-      csr privkey issuer in
+      csr (`RSA privkey) issuer in
   let cert_pem = X509.Encoding.Pem.Certificate.to_pem_cstruct1 cert in
-  let key_pem = X509.Encoding.Pem.Private_key.to_pem_cstruct1 privkey in
+  let key_pem = X509.Encoding.Pem.Private_key.to_pem_cstruct1 (`RSA privkey) in
   match (write_pem certfile cert_pem, write_pem keyfile key_pem) with
   | Ok, Ok -> `Ok
   | Error str, _ | _, Error str -> Printf.eprintf "%s\n" str; `Error
