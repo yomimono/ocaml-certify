8fe942b6e94f9c6bd8d66e72fe9c24e711009e73
Hannes Mehnert <hannes@mehnert.org>
2020-01-27 13:10:07 +0100

    adapt to X.509 0.9.0 API changes

--- src/common.ml
+++ src/common.ml
@@ -58,8 +58,7 @@ let sign days key pubkey issuer csr names entity =
     | `RSA priv, `RSA pub when Nocrypto.Rsa.pub_of_priv priv = pub ->
       let info = X509.Signing_request.info csr in
       let extensions = extensions info.X509.Signing_request.public_key pubkey names entity in
-      let cert = X509.Signing_request.sign ~valid_from ~valid_until ~extensions csr key issuer in
-      Ok cert
+      X509.Signing_request.sign ~valid_from ~valid_until ~extensions csr key issuer
     | _ -> Error (`Msg "public / private keys do not match")
 
 let read_pem src =
